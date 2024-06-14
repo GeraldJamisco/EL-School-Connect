8097b9d0a60bdfad6852042323f75e0d2b035914
GeraldJamisco <72699531+GeraldJamisco@users.noreply.github.com>
2024-06-14 15:01:08 +0300

    Initial commit

---
+++ admin/index.php
@@ -0,0 +1,101 @@
+<?php require_once('header.php'); ?>
+
+<section class="content-header">
+	<h1>Dashboard</h1>
+</section>
+
+<?php
+
+$statement = $pdo->prepare("SELECT * FROM tbl_product");
+$statement->execute();
+$total_product = $statement->rowCount();
+
+$statement = $pdo->prepare("SELECT * FROM tbl_customer WHERE cust_status='1'");
+$statement->execute();
+$total_customers = $statement->rowCount();
+
+$statement = $pdo->prepare("SELECT * FROM tbl_payment WHERE payment_status=?");
+$statement->execute(array('Completed'));
+$total_order_completed = $statement->rowCount();
+
+
+$statement = $pdo->prepare("SELECT * FROM tbl_payment WHERE payment_status=?");
+$statement->execute(array('Pending'));
+$total_order_pending = $statement->rowCount();
+
+?>
+
+<section class="content">
+<div class="row">
+            <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-primary">
+                <div class="inner">
+                  <h3><?php echo $total_product; ?></h3>
+
+                  <p>Products</p>
+                </div>
+                <div class="icon">
+                  <i class="ionicons ion-android-cart"></i>
+                </div>
+                
+              </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-maroon">
+                <div class="inner">
+                  <h3><?php echo $total_order_pending; ?></h3>
+
+                  <p>Pending Orders</p>
+                </div>
+                <div class="icon">
+                  <i class="ionicons ion-clipboard"></i>
+                </div>
+                
+              </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-xs-6">
+              <!-- small box -->
+              <div class="small-box bg-green">
+                <div class="inner">
+                  <h3><?php echo $total_order_completed; ?></h3>
+
+                  <p>Completed Orders</p>
+                </div>
+                <div class="icon">
+                  <i class="ionicons ion-android-checkbox-outline"></i>
+                </div>
+               
+              </div>
+            </div>
+            <!-- ./col -->
+        
+			
+
+			  <div class="col-lg-3 col-xs-6">
+				<!-- small box -->
+				<div class="small-box bg-red">
+				  <div class="inner">
+					<h3><?php echo $total_customers; ?></h3>
+  
+					<p>Active Customers</p>
+				  </div>
+				  <div class="icon">
+					<i class="ionicons ion-person-stalker"></i>
+				  </div>
+				  
+				</div>
+			  </div>
+
+
+
+					 
+
+		  </div>
+		  
+</section>
+
+<?php require_once('footer.php'); ?>
